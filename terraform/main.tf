2c55b734d448f4aa69966eb282d18e15da4bfbbd
Golmohammadi <mohammadmahdi.golmohammadi@capgemini.com>
2024-11-14 12:02:26 +0100

    6

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,11 +73,9 @@ resource "null_resource" "link_monitoring" {
       # TODO your scripting code
     EOT
     environment = {
-      // Parameters needed to login
       con_client_id     = var.client_id
       con_client_secret = var.client_secret
       con_tenant_id     = var.tenant_id
-      // Parameters needed for linking
       inst_key          = azurerm_application_insights.appi.instrumentation_key
       conn_str          = azurerm_application_insights.appi.connection_string     
       rg_name           = var.rg_name
